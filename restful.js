6db31b5c7f7421466dbd10c0298c2eb09ea79f95
SEBASTIAN PRIETO <prietosebby@gmail.com>
2023-05-09 13:41:55 -0400

    updated

--- restful.js
+++ restful.js
@@ -5,6 +5,35 @@ import pg from "pg";
 
 const app = express();
 const PORT = 4000;
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+app.use((req, res, next) => {
+  //SUPER BASIC just for learning purposes
+  const auth = { login: "admin", password: "meowmix" }; // change this
+
+  // parse login and password from headers
+  const b64auth = (req.headers.authorization || "").split(" ")[1] || "";
+  console.log(req.headers.authorization);
+  console.log(b64auth);
+  const [login, password] = Buffer.from(b64auth, "base64")
+    .toString()
+    .split(":");
+  console.log(login, password);
+  // Verify login and password are set and correct
+  if (login && password && login === auth.login && password === auth.password) {
+    // Access granted...
+    return next();
+  }
+
+  // Access denied...
+  res.set("WWW-Authenticate", 'Basic realm="401"'); // change this
+  res.status(401).send("Authentication required."); // custom message
+});
+
 app.use(express.json());
 const db = new pg.Pool({
   database: "petshop",
@@ -16,6 +45,23 @@ app.get("/", (req, res) => {
       throw error;
     }
     res.send(result.rows);
+  });
+});
+
+app.get("/pets/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  if (Number.isNaN(id)) {
+    res.sendStatus(422);
+    return;
+  }
+  db.query("SELECT * FROM pet WHERE id = $1", [id]).then((result) => {
+    // console.log(result.query);
+    if (result.rows.length === 0) {
+      res.sendStatus(404);
+    } else {
+      res.send(result.rows[0]);
+    }
   });
 });
 
